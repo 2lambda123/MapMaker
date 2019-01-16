41dab48cfc77db5cfcc064b0f0f37012f68692b5
ahuynh15 <alvin.huynh.01@gmail.com>
2019-01-15 21:51:22 -0500

    Added placeholder UI and modified input manager

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
 
 public class InputManager : MonoBehaviour
 {
 	[SerializeField]
+	[Tooltip("The tilemap that will be edited in the scene")]
 	private Tilemap tilemap = null;
 
 	[SerializeField]
+	[Tooltip("The selected tile that will be placed in the scene")]
 	private TileBase selectedTile;
+
+	[Header("Debug Settings")]
+	[SerializeField]
+	private bool mousePressStartedOnUI;
 
 	// Start is called before the first frame update
 	void Start()
@@ -20,16 +27,37 @@ public class InputManager : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		// Whenever the left-mouse button is held down, update the cell under the cursor depending on the selected tile
+		// Set a flag wheneven the left-mouse button is pressed down while above the UI
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (EventSystem.current.IsPointerOverGameObject())
+			{
+				mousePressStartedOnUI = true;
+			}
+		}
+
+		// Whenever the left-mouse button is held down above a tile, update the cell under the cursor depending on the selected tile
 		if (Input.GetMouseButton(0))
 		{
-			// Calculate the position of the cell being clicked on
-			Vector3Int clickedCell = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+			// Modify the tile map when the following conditions are satisfied:
+			// 1. The mouse cursor is NOT above a UI element
+			// 2. The mouse click did NOT begin on a UI element
+			if (!EventSystem.current.IsPointerOverGameObject() && !mousePressStartedOnUI)
+			{
+				// Calculate the position of the cell being clicked on
+				Vector3Int clickedCell = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
 
-			Debug.Log(Input.mousePosition + "-> Clicked on coordinates " + clickedCell);
+				Debug.Log(Input.mousePosition + "-> Clicked on coordinates " + clickedCell);
 
-			// Update the selected cell with the seleced tile
-			tilemap.SetTile(clickedCell, selectedTile);
+				// Update the selected cell with the seleced tile
+				tilemap.SetTile(clickedCell, selectedTile);
+			}
+		}
+
+		// Release all flags wheneven the left-mouse button is released
+		if (Input.GetMouseButtonUp(0))
+		{
+			mousePressStartedOnUI = false;
 		}
 	}
 }
