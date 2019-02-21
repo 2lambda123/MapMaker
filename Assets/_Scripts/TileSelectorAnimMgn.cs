e4ab1d22ba45ae8c687ffedb9c566f4bbb5082d8
FirstNameNate <38254433+FirstNameNate@users.noreply.github.com>
2019-02-21 14:08:44 -0500

    TileSelectionPanel - Animated

---
+++ Assets/_Scripts/TileSelectorAnimMgn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSelectorAnimMgn : MonoBehaviour
+{
+    Animator anim;
+
+    //variable for checking if the menu is open 
+    private bool isOpen = true;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void tileSelectorTab()
+    {
+        //open if closed
+        if (!isOpen)
+        {
+            isOpen = true;
+            slideIn();
+        }
+        //close if opened
+        else if (isOpen)
+        {
+            isOpen = false;
+            slideOut();
+        }
+    }
+
+    public void slideOut()
+    {
+        anim.SetTrigger("SlideOut");
+    }
+
+    public void slideIn()
+    {
+        anim.SetTrigger("SlideIn");
+    }
+}
+
+
+
+//SDG
