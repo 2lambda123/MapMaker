90d883bccd6275aed62c40654f9145daa18c6d4b
Alvin Huynh <alvin.huynh.01@gmail.com>
2019-03-04 12:35:22 -0500

    Presentation Prep
    Modifying objects and hiding incomplete features for presentation

--- Assets/_Scripts/cameraInteraction.cs
+++ Assets/_Scripts/cameraInteraction.cs
@@ -4,82 +4,82 @@ using UnityEngine;
 
 public class cameraInteraction : MonoBehaviour
 {
-	[SerializeField]
-	private float speed;
+    [SerializeField]
+    private float speed;
 
-	private int canvasHeight = default;
-	private int canvasWidth = default;
+    private int canvasHeight = default;
+    private int canvasWidth = default;
 
-	private const float TILE_SIZE = 2.5f;
+    private const float TILE_SIZE = 2.56f;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		Debug.Log("Camera Start Script");
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Camera Start Script");
 
-		canvasHeight = PlayerPrefs.GetInt("Canvas Height", 64);
-		canvasWidth = PlayerPrefs.GetInt("Canvas Width", 64);
-	}
+        canvasHeight = PlayerPrefs.GetInt("Canvas Height", 64);
+        canvasWidth = PlayerPrefs.GetInt("Canvas Width", 64);
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		//move camera directionally
-		//conditional statements for each of the arrow keys
-		//note the hardcoded limits (3's and -3's) will need to be replaced with variables for different sized canvases
-		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-		{
-			Debug.Log(Camera.main.transform.position[1]);
-			Debug.Log("up arrow");
-			if (Camera.main.transform.position[1] < canvasHeight * TILE_SIZE)
-			{
-				transform.Translate(Vector2.up * speed * Time.deltaTime);
-			}
-		}
-		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-		{
-			Debug.Log("down arrow");
-			if (Camera.main.transform.position[1] > 0)
-			{
-				transform.Translate(Vector2.down * speed * Time.deltaTime);
-			}
-		}
-		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-		{
-			Debug.Log("right arrow");
-			if (Camera.main.transform.position[0] < canvasWidth * TILE_SIZE)
-			{
-				transform.Translate(Vector2.right * speed * Time.deltaTime);
-			}
-		}
-		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-		{
-			Debug.Log("left arrow");
-			if (Camera.main.transform.position[0] > 0)
-			{
-				transform.Translate(Vector2.left * speed * Time.deltaTime);
-			}
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        //move camera directionally
+        //conditional statements for each of the arrow keys
+        //note the hardcoded limits (3's and -3's) will need to be replaced with variables for different sized canvases
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            Debug.Log(Camera.main.transform.position[1]);
+            Debug.Log("up arrow");
+            if (Camera.main.transform.position[1] < canvasHeight * TILE_SIZE)
+            {
+                transform.Translate(Vector2.up * speed * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("down arrow");
+            if (Camera.main.transform.position[1] > 0)
+            {
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("right arrow");
+            if (Camera.main.transform.position[0] < canvasWidth * TILE_SIZE)
+            {
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("left arrow");
+            if (Camera.main.transform.position[0] > 0)
+            {
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
+            }
+        }
 
-		//zoom camera with key presses "-" decrement, "=" for increment
-		if (Input.GetKey(KeyCode.Equals))
-		{
-			Debug.Log("plus");
-			if (Camera.main.orthographicSize > 4)
-			{
-				Camera.main.orthographicSize -= 1;
-			}
+        //zoom camera with key presses "-" decrement, "=" for increment
+        if (Input.GetKey(KeyCode.Equals))
+        {
+            Debug.Log("plus");
+            if (Camera.main.orthographicSize > 4)
+            {
+                Camera.main.orthographicSize -= 1;
+            }
 
 
-		}
-		if (Input.GetKey(KeyCode.Minus))
-		{
-			Debug.Log("minus");
-			if (Camera.main.orthographicSize < 26)
-			{
-				Camera.main.orthographicSize += 1;
-			}
+        }
+        if (Input.GetKey(KeyCode.Minus))
+        {
+            Debug.Log("minus");
+            if (Camera.main.orthographicSize < 26)
+            {
+                Camera.main.orthographicSize += 1;
+            }
 
-		}
-	}
+        }
+    }
 }
